4a37ec89f451879d60bdac4fcdcc3a5cf3244514
katansapdevelop <katan.sap.development@gmail.com>
2016-06-30 21:50:48 +1000

    New App Structure

---
+++ controller/Root.controller.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+
+    sap.ui.controller("com.katan.view.Root", {
+        onInit: function() {
+            this.app = this.getView().byId("App");
+        }
+
+
+    });
+
+}());
